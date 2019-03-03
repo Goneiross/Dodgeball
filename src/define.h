6087f92a92491fcaa87b1b016da9ab0dd9f97680
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-03-03 09:51:06 +0100

    Add define.h from project materials

---
+++ src/define.h
@@ -0,0 +1,12 @@
+#define MIN_CELL                       3
+#define MAX_TOUCH                      4
+#define MAX_COUNT                      20
+#define DELTA_T                        0.0625 
+#define DIM_MAX                        400 
+#define SIDE                           (2*DIM_MAX)
+#define COEF_RAYON_JOUEUR               0.25
+#define COEF_VITESSE_JOUEUR             1.0
+#define COEF_RAYON_BALLE                0.125
+#define COEF_VITESSE_BALLE              1.25 
+#define COEF_MARGE_JEU                  (1./256)
+#define MAXLINE                         8
