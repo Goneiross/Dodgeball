fbe1e080c5a5ad471d52fc1df5269ae946dc11f3
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-03-03 14:33:35 +0100

    Add Player class

--- src/player.cc
+++ src/player.cc
@@ -1,4 +1,31 @@
+#include <math.h>
 #include "player.h"
 #include "tools.h"
+#include "define.h"
 
 using namespace std;
+
+class Player {
+    public :
+        Player(int x, int y,  int nbCell);
+        void updatePosition(double angle);
+        ~Player();
+    private :
+        double xPosition, yPosition;
+        double radius;
+        int nbt;
+        double count;
+        double velocity;
+};
+
+Player::Player(int x0, int y0, int nbCell){
+    xPosition = x0;
+    yPosition = y0;
+    radius = COEF_VITESSE_JOUEUR * (SIDE / nbCell);
+    velocity = COEF_VITESSE_JOUEUR * (SIDE / nbCell);
+}
+
+void Player::updatePosition(double angle){
+    xPosition += cos(angle) * velocity;
+    yPosition += sin(angle) * velocity;
+}
