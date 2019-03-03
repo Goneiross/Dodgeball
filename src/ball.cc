ba0d550db8777acb4eee5c9663270076600c4e09
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-03-03 15:31:33 +0100

    Correct Ball class (now in the right place) and use now use properly header

--- src/ball.cc
+++ src/ball.cc
@@ -1,6 +1,18 @@
+#include <math.h>
+#include "define.h"
 #include "ball.h"
 #include "tools.h"
 
 using namespace std;
 
-class ball{};
+Ball::Ball(int x0, int y0, int nbCell){
+    xPosition = x0;
+    yPosition = y0;
+    radius = COEF_VITESSE_BALLE * (SIDE / nbCell);
+    velocity = COEF_VITESSE_BALLE * (SIDE / nbCell);
+}
+
+void Ball::updatePosition(double angle){
+    xPosition += cos(angle) * velocity;
+    yPosition += sin(angle) * velocity;
+}
--- src/player.cc
+++ src/player.cc
@@ -3,26 +3,3 @@
 #include "define"
 
 using namespace std;
-
-class Ball {
-    public :
-        Ball(int x, int y,  int nbCell);
-        void updatePosition(double angle);
-        ~Ball();
-    private :
-        double xPosition, yPosition;
-        double radius;
-        double velocity;
-};
-
-Ball::Ball(int x0, int y0, int nbCell){
-    xPosition = x0;
-    yPosition = y0;
-    radius = COEF_VITESSE_BALLE * (SIDE / nbCell);
-    velocity = COEF_VITESSE_BALLE * (SIDE / nbCell);
-}
-
-void Ball::updatePosition(double angle){
-    xPosition += cos(angle) * velocity;
-    yPosition += sin(angle) * velocity;
-}
