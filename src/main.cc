b3b531c9050b7985df5b4511d9b0c2b0218f96ab
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-03-03 11:49:36 +0100

    Add interpretation of user's arguments Including : Error, Step, pathToFile, --help (not entirely done), and gestion of number of arguments

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include "GUI.h"
 #include "simulation.h"
@@ -6,5 +7,27 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
+    int mode = 0; // Maybe create a function returning struct with Mode and Inputfile
+    string inputFile = ""; // Better with string or char* ?
+    if (argc == 1){
+        cout << "Type '--help' for a list of commands" << endl; // #AskBoulic for cout
+    } else if (argc < 4) {
+        for (int i = 1; i < argc; i++){
+            if (string(argv[i]) == "Error"){
+                mode = 1;
+            
+            } else if (string(argv[i]) == "Step"){
+                mode = 2;
+            } else if(string(argv[i]) == "--help") { // Maybe quit program after --help ? break ?
+                cout << "" << endl; // To do
+            } else {          // #AskBoulic for extension txt
+                inputFile = argv[i];
+            }
+        }
+    } else {
+        cout << "Error, too many arguments, type '--help' for help" << endl;
+    }
+    cout << "Mode : " << mode << endl; // #AskBoulic for cout
+    cout << "InputFile : " << inputFile << endl; // #AskBoulic for cout
     return 0;
 }
