124865c9149e2b21e2c93b40e6e3124afbe6343f
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-03-04 14:23:55 +0100

    Add getters for ball and correct radius

--- src/ball.cc
+++ src/ball.cc
@@ -8,11 +8,13 @@ using namespace std;
 Ball::Ball(int x0, int y0, int nbCell){
     xPosition = x0;
     yPosition = y0;
-    radius = COEF_VITESSE_BALLE * (SIDE / nbCell);
+    radius = COEF_RAYON_BALLE* (SIDE / nbCell);
     velocity = COEF_VITESSE_BALLE * (SIDE / nbCell);
 }
-
 void Ball::updatePosition(double angle){
     xPosition += cos(angle) * velocity;
     yPosition += sin(angle) * velocity;
 }
+double Ball::x () const {return xPosition;}
+double Ball::y () const {return yPosition;}
+double Ball::getRadius () const {return radius;}
--- src/ball.h
+++ src/ball.h
@@ -1,8 +1,11 @@
 class Ball {
     public :
         Ball(int x, int y,  int nbCell);
-        void updatePosition(double angle);
         ~Ball();
+        double x () const;
+        double y () const;
+        double getRadius () const;
+        void updatePosition(double angle);
     private :
         double xPosition, yPosition;
         double radius;
