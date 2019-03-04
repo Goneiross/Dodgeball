f21ca985b2b2f95b5f52cb31842a398ff0e8ff5a
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-03-04 14:31:07 +0100

    Add getters and small fixes

--- src/player.cc
+++ src/player.cc
@@ -6,9 +6,6 @@
 
 using namespace std;
 
-double Player::x() const{return xPosition;};
-double Player::y() const{return yPosition;};
-
 Player::Player(double x0, double y0, int nbCell){
     xPosition = x0;
     yPosition = y0;
@@ -16,14 +13,19 @@ Player::Player(double x0, double y0, int nbCell){
     velocity = COEF_VITESSE_JOUEUR * (SIDE / nbCell);
 }
 
+double Player::x() const{return xPosition;};
+double Player::y() const{return yPosition;};
+double Player::getRadius() const{return radius;}
+double Player::getCount() const{return count;}
+double Player::getTimeTouched() const{return timeTouched;}
+
 void Player::updatePosition(double angle){
     xPosition += cos(angle) * velocity;
     yPosition += sin(angle) * velocity;
 }
-
 bool Player::touchedAndDead(){
-    count += 1;
-    if (count == MAX_COUNT) { //Dead when equal or sup ? //#askBoulic
+    timeTouched += 1;
+    if (timeTouched == MAX_TOUCH) { //Dead when equal or sup ? //#askBoulic
         return(true);
     }
     return(false);
--- src/player.h
+++ src/player.h
@@ -1,15 +1,18 @@
 class Player {
     public :
-        Player(int x, int y,  int nbCell);
+        Player(double x, double y,  int nbCell);
+        ~Player(){};
+        double x() const;
+        double y() const;
+        double getRadius() const;
+        double getCount() const;
+        double getTimeTouched() const;
         void updatePosition(double angle);
         bool touchedAndDead();
-        ~Player(){};
-        int x();
-        int y();
     private :
         double xPosition, yPosition;
         double radius;
-        int nbt;
+        int timeTouched;
         double count;
         double velocity;
 };
