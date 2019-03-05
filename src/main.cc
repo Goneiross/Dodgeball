578a1c26c940c88253e754c29a9b8961379e57e7
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-03-05 16:41:12 +0100

    Update file parsing

--- src/main.cc
+++ src/main.cc
@@ -7,10 +7,6 @@
 
 using namespace std;
 
-int readFile(){
-    
-}
-
 int main(int argc, char* argv[]){
     int mode = 0; // Maybe create a function returning struct with Mode and Inputfile
     string inputFile = ""; // Better with string or char* ?
@@ -32,18 +28,50 @@ int main(int argc, char* argv[]){
     } else {
         cout << "Error, too many arguments, type '--help' for help" << endl;
     }
+    
+    int nbCell, nbPlayer, nbObstacle, nbBall;
 
-    ifstream flux;
-    flux.open(inputFile);
+    ifstream flux (inputFile, ios::in);
     if (!flux) {
         cout << "Unable to open file datafile.txt"; // Maybe better with cerr
-        exit(1); 
+        exit(0); 
     }
-    string x;
-    while(flux >> x){
-        cout << x << endl;
+    string tmp, tmp1, tmp2, tmp3;
+    while(flux){
+        flux >> tmp;
+        if (tmp == "#"){
+            do {
+                flux >> tmp;
+            } while (tmp != "#"); // Is there a # at the end of the comment in every case ???
+        }
+        flux >> nbCell;
+        flux >> nbPlayer;
+        Player* players[nbPlayer];
+        for (int i = 0; i < nbPlayer; i++){
+            flux >> tmp;
+            flux >> tmp1;
+            flux >> tmp2;
+            flux >> tmp3;
+            Players[i] = new Player(tmp, tmp1, tmp2, tmp3);
+        }
+        flux >> nbObstacle;
+        Map mainMap = new Map(nbCell, nbCell);
+        for (int i = 0; i < nbObstacle; i++){
+            flux >> tmp;
+            flux >> tmp1;
+            addObstacle(tmp, tmp1);
+        }
+        flux >> nbBall;
+        Ball* balls[nbBall];
+        for (int i = 0; i < nbBall; i++){
+            flux >> tmp;
+            flux >> tmp1;
+            flux >> tmp2;
+            balls[i] = new Ball(tmp, tmp1, tmp2, nbCell);
+        }
     }
     flux.close();
+
 
     return 0;
 }
