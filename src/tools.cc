01fc6ee4bf0af4e8553af91d08e57ae03eef0028
Quelqunquiteveutdubien <gllmmch77@gmail.com>
2019-03-05 16:47:05 +0100

    added space after the '/' in divisions commented one possible simplification  in tools.cc changed ".x" and ".y" with .getx" and ".gety" in tools.h

--- src/tools.cc
+++ src/tools.cc
@@ -9,8 +9,8 @@ Circle::Circle(double x, double y, double r){
     radius = r;
 }
 double Circle::getRadius () const {return radius;}
-double Circle::x () const {return xPosition;}
-double Circle::y () const {return yPosition;}
+double Circle::getx () const {return xPosition;}
+double Circle::gety () const {return yPosition;}
 
 Square::Square(double x, double y, double s){
     xPosition = x;
@@ -18,8 +18,8 @@ Square::Square(double x, double y, double s){
     side = s;
 }
 double Square::getSide () const {return side;}
-double Square::x () const {return xPosition;}
-double Square::y () const {return yPosition;}
+double Square::getx () const {return xPosition;}
+double Square::gety () const {return yPosition;}
 
 // Need to optimize distance computation
 // For example do a generic function computing distance between two points
@@ -36,14 +36,14 @@ double distance(Circle c1, Circle c2){
 double distance(Square s1, Square s2){
     double x = abs(s2.x - s1.x);
     double y = abs(s2.y - s1.y);
-    double angle = atan(y /x);
+    double angle = atan(y / x);
     double included = (s1.getSide / 2 + s2.getSide / 2) / cos(angle);
     return sqrt(pow(x, 2) + pow(y, 2)) - included;
 }
-double distance(Square s, Circle c){
-    double x = abs(c.x - s.x);
-    double y = abs(c.y - s.y);
-    double angle = atan(y /x);
+double distance(Square s, Circle c){    // other possibility: make one of those functions call the other
+    double x = abs(c.x - s.x);          //advantage: shorter code
+    double y = abs(c.y - s.y);          //disadvantage: one more function call
+    double angle = atan(y / x);
     double included = (c.getRadius / 2 + s.getSide / 2) / cos(angle);
     return sqrt(pow(x, 2) + pow(y, 2)) - included;
 }
