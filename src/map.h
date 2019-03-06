8664e70b9110d7d5f0aa11cd0cdbaf333f54afff
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-03-06 13:28:51 +0100

    Correct map destructor

--- src/map.h
+++ src/map.h
@@ -1,7 +1,7 @@
 class Map {
     public :
         Map(int xSize, int ySize);
-        ~Map();
+        ~Map(){};
         void addObstacle(int xPosition, int yPosition);
         void removeObstacle(int xPosition, int yPosition);
         bool isObstacle(double x, double y) const;
