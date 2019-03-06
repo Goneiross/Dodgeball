81ea0c153e3c6001c7dbdafc33cecbc5d6bc5439
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-03-06 11:51:48 +0100

    Fix method call

--- src/tools.cc
+++ src/tools.cc
@@ -31,25 +31,25 @@ double distance(double x1, double y1, double x2, double y2){
     return sqrt(pow(distance(x1, x2), 2) + pow(distance(y1, y2), 2));
 }
 double distance(Circle c1, Circle c2){ 
-    return distance(c1.x, c1.y, c2.x, c2.y) - c1.getRadius - c2.getRadius;
+    return distance(c1.getX, c1.getY, c2.getX, c2.getY) - c1.getRadius - c2.getRadius;
 }
 double distance(Square s1, Square s2){
-    double x = abs(s2.x - s1.x);
-    double y = abs(s2.y - s1.y);
+    double x = abs(s2.getX - s1.getX);
+    double y = abs(s2.getY - s1.getY);
     double angle = atan(y / x);
     double included = (s1.getSide / 2 + s2.getSide / 2) / cos(angle);
     return sqrt(pow(x, 2) + pow(y, 2)) - included;
 }
 double distance(Square s, Circle c){    // other possibility: make one of those functions call the other
-    double x = abs(c.x - s.x);          //advantage: shorter code
-    double y = abs(c.y - s.y);          //disadvantage: one more function call
+    double x = abs(c.getX - s.getX);          //advantage: shorter code
+    double y = abs(c.getY - s.getY);          //disadvantage: one more function call
     double angle = atan(y / x);
     double included = (c.getRadius / 2 + s.getSide / 2) / cos(angle);
     return sqrt(pow(x, 2) + pow(y, 2)) - included;
 }
 double distance(Circle c, Square s){
-    double x = abs(c.x - s.x);
-    double y = abs(c.y - s.y);
+    double x = abs(c.getX - s.getX);
+    double y = abs(c.getY - s.getY);
     double angle = atan(y /x);
     double included = (c.getRadius / 2 + s.getSide / 2) / cos(angle);
     return sqrt(pow(x, 2) + pow(y, 2)) - included;
