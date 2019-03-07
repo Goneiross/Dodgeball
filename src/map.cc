1c5ac9a2a45785186ffdc3c5e9b7084c4de3e6a8
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-03-07 09:08:31 +0100

    Correct grid initialisation

--- src/map.cc
+++ src/map.cc
@@ -4,13 +4,13 @@
 using namespace std;
 
 Map::Map(int xSize, int ySize){
-    bool** grid = new bool*[xSize];
-        for (int i = 0; i < xSize; i++) {
-            grid[i] = new bool[ySize];
-            for (int j = 0; j < ySize; j++) {
-                grid[i][j] = false;
-            }
+    grid = new bool*[10];
+    for (int i = 0; i < 10; i++) {
+        grid[i] = new bool[10];
+        for (int j = 0; j < 10; j++) {
+            grid[i][j] = false;
         }
+    }
 }
 void Map::addObstacle(int xPosition, int yPosition){
     grid[xPosition][yPosition] = true;
--- src/map.h
+++ src/map.h
@@ -5,6 +5,7 @@ class Map {
         void addObstacle(int xPosition, int yPosition);
         void removeObstacle(int xPosition, int yPosition);
         bool isObstacle(double x, double y) const;
+        void test();
     private :
-        bool** grid;
+        bool **grid;
 };
