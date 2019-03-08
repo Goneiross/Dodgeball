7f80284c89d96741ba8e707fc47a6e0f7c6e7f11
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-03-08 15:32:30 +0100

    Fix parsing data from input file Now works great with comments

--- src/simulation.cc
+++ src/simulation.cc
@@ -18,37 +18,46 @@ void simulation(std::string inputFile){
     }
     
     string tmp0, tmp1, tmp2, tmp3;
+    int part = 0, p = 0, o = 0, b = 0;
+    char tmp;
+    Map* mainMap = new Map(nbCell, nbCell); //Is this correct ? - could modify the class to take only one argument (squared map)
+    
     while(flux){
         flux >> tmp0;
         if (tmp0 == "#"){
-            do {
-                flux >> tmp0;
-            } while (tmp0 != "#"); // Is there a # at the end of the comment in every case ???
-        }
-        flux >> nbCell;
-        flux >> nbPlayer;
-        Player* players[nbPlayer];
-        for (int i = 0; i < nbPlayer; i++){
-            flux >> tmp0;
-            flux >> tmp1;
-            flux >> tmp2;
-            flux >> tmp3;
-            players[i] = new Player(stod(tmp0), stod(tmp1), stoi(tmp2), stod(tmp3), nbCell);
-        }
-        flux >> nbObstacle;
-        Map* mainMap = new Map(nbCell, nbCell); //Is this correct ? - could modify the class to take only one argument (squared map)
-        for (int i = 0; i < nbObstacle; i++){
-            flux >> tmp0;
+            do {flux.get(tmp);} while (tmp != '\n');
+        } else if(part == 0){
+            nbCell = stoi(tmp0);
+            part++;
+        } else if (part == 1){
+            nbPlayer = stoi(tmp0);
+            part++;
+        } else if (part == 2){
+            Player* players[nbPlayer];
+            flux >> tmp1 >> tmp2 >> tmp3;
+            players[p] = new Player(stod(tmp0), stod(tmp1), stoi(tmp2), stod(tmp3), nbCell);
+            p ++;
+            if (p == nbPlayer){part++;}
+        } else if (part == 3){
+            nbObstacle = stoi(tmp0);
+            part++;
+        } else if (part == 4){
             flux >> tmp1;
             mainMap->addObstacle(stod(tmp0), stod(tmp1));
-        }
-        flux >> nbBall;
-        Ball* balls[nbBall];
-        for (int i = 0; i < nbBall; i++){
-            flux >> tmp0;
-            flux >> tmp1;
-            flux >> tmp2;
-            balls[i] = new Ball(stod(tmp0), stod(tmp1), stod(tmp2), nbCell);
+            o++;
+            if (o == nbObstacle){part++;}
+        } else if (part == 5){
+            nbBall = stoi(tmp0);
+            part++;
+        } else if (part == 6){
+            Ball* balls[nbBall];
+            flux >> tmp1 >> tmp2;
+            balls[b] = new Ball(stod(tmp0), stod(tmp1), stod(tmp2), nbCell);
+            b++;
+            if (b == nbBall){part++;}
+        } else {
+            cout << "t" << endl;
+            flux.get(tmp);
         }
     }
     flux.close();
