9f05334289cb9c87292c9e3f8e07125d46dfa2f9
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-03-10 16:41:21 +0100

    Update Distance computation : now from center to center

--- src/tools.cc
+++ src/tools.cc
@@ -21,36 +21,21 @@ double Square::getSide() const {return side;}
 double Square::getX() const {return xPosition;}
 double Square::getY() const {return yPosition;}
 
-// Need to optimize distance computation
-// For example do a generic function computing distance between two points
-
-double distance(double x, double y){
-    return abs(x - y);
+double distance(double x1, double x2){
+    return abs(x1 - x2);
 }
 double distance(double x1, double y1, double x2, double y2){
-    return sqrt(pow(distance(x1, x2), 2) + pow(distance(y1, y2), 2));
+    return sqrt(pow(distance(x2, x1), 2) + pow(distance(y2, y1), 2));
 }
 double distance(Circle c1, Circle c2){ 
-    return distance(c1.getX(), c1.getY(), c2.getX(), c2.getY()) - c1.getRadius() - c2.getRadius();
+    return distance(c1.getX(), c1.getY(), c2.getX(), c2.getY());
 }
 double distance(Square s1, Square s2){
-    double x = abs(s2.getX() - s1.getX());
-    double y = abs(s2.getY() - s1.getY());
-    double angle = atan(y / x);
-    double included = (s1.getSide() / 2 + s2.getSide() / 2) / cos(angle);
-    return sqrt(pow(x, 2) + pow(y, 2)) - included;
+    return distance(s1.getX(), s1.getY(), s2.getX(), s2.getY());
 }
-double distance(Square s, Circle c){    // other possibility: make one of those functions call the other
-    double x = abs(c.getX() - s.getX());          //advantage: shorter code
-    double y = abs(c.getY() - s.getY());          //disadvantage: one more function call
-    double angle = atan(y / x);
-    double included = (c.getRadius() / 2 + s.getSide() / 2) / cos(angle);
-    return sqrt(pow(x, 2) + pow(y, 2)) - included;
+double distance(Square s, Circle c){
+    return distance(s.getX(), s.getY(), c.getX(), c.getY());
 }
 double distance(Circle c, Square s){
-    double x = abs(c.getX() - s.getX());
-    double y = abs(c.getY() - s.getY());
-    double angle = atan(y /x);
-    double included = (c.getRadius() / 2 + s.getSide() / 2) / cos(angle);
-    return sqrt(pow(x, 2) + pow(y, 2)) - included;
+    return distance(s.getX(), s.getY(), c.getX(), c.getY());
 }
