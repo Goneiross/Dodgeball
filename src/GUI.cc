71c06edae4e9261e2c54a429ed7b7d5eee573863
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-03-31 20:36:06 +0200

    Add a test drawing area

--- src/GUI.cc
+++ src/GUI.cc
@@ -11,6 +11,96 @@
 
 using namespace Gtk;
 
+class MyArea: public Gtk::DrawingArea {
+  public:
+    MyArea(){};
+    virtual ~MyArea(){};
+    void clear();
+    void draw();
+  protected:
+    bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;
+  private:
+    bool empty;
+    void refresh();
+};
+
+void MyArea::clear(){
+  empty= true;
+  refresh();
+}
+void MyArea::draw(){
+  empty= false;
+  refresh();
+}
+void MyArea::refresh(){
+  auto win = get_window();
+  if(win){
+    Gdk::Rectangle r(0,0, get_allocation().get_width(), get_allocation().get_height());
+    win->invalidate_rect(r,false);
+  }
+}
+
+bool MyArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr){
+  if(not empty) {
+    // ici dessin comme avant
+    cr->set_line_width(50.0);
+
+  // E
+  cr->set_source_rgb(0.92, 0.0, 0.0);
+  cr->move_to(50, 50);
+  cr->line_to(50, 150);
+  cr->stroke();
+  cr->move_to(50, 200);
+  cr->line_to(50, 300);
+  cr->stroke();
+  cr->move_to(50, 275);
+  cr->line_to(190, 275);
+  cr->stroke();
+  cr->move_to(73, 175);//E milieu
+  cr->line_to(190, 175);
+  cr->stroke();
+  cr->move_to(75, 75);
+  cr->line_to(190, 75);
+  cr->stroke();
+  
+  //P
+  cr->move_to(250, 50);
+  cr->line_to(250, 300);
+  cr->stroke();
+  cr->move_to(275, 75);
+  cr->line_to(325, 75);
+  cr->stroke();
+  cr->move_to(275, 175);
+  cr->line_to(325, 175);
+  cr->stroke();
+  cr->arc(325, 125, 50, -M_PI/2, M_PI/2);
+  cr->stroke();
+  
+  //F
+  cr->move_to(450, 50);
+  cr->line_to(450, 150);
+  cr->stroke();
+  cr->move_to(450, 200);
+  cr->line_to(450, 300);
+  cr->stroke();
+  cr->move_to(450, 75);//F haut
+  cr->line_to(590, 75);
+  cr->stroke();
+  cr->move_to(473, 175);//F milieu
+  cr->line_to(575, 175);
+  cr->stroke();
+
+//L
+  cr->move_to(650, 50);
+  cr->line_to(650, 300);
+  cr->stroke();
+  cr->move_to(650, 275);
+  cr->line_to(790, 275);
+  cr->stroke();
+  } else { std::cout << "Empty !" << std::endl; }
+  return true;
+}
+
 class GUI: public Window {
   public:
     GUI();
@@ -28,6 +118,7 @@ class GUI: public Window {
     Button m_button_start;
     Button m_button_step;
     Label m_label_status;
+    MyArea m_area;
 };
 
 GUI::GUI(): 
@@ -45,9 +136,14 @@ GUI::GUI():
   set_border_width(10);
   maximize();
 
+  m_box1.set_size_request(100, 100);
+
   add(m_box_top);
   m_box_top.pack_start(m_box1);
   m_box_top.pack_start(m_box2);
+
+  m_area.set_size_request(200,200);
+  m_box_top.pack_start(m_area);
 
   m_box1.pack_start(m_button_exit);
   m_box1.pack_start(m_button_open);
