4ce50ce0eafba3a6894db77a44c86080649f4e22
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-04-04 20:54:59 +0200

    Update draw function to now draw players

--- src/GUI.h
+++ src/GUI.h
@@ -4,5 +4,10 @@
   \date   March 2019
   \brief  "GUI" module header
 */
-
-int draw();
+#ifndef OBJECT_HEADER
+#define OBJECT_HEADER
+#include "player.h"
+#include "ball.h"
+#include "map.h"
+#endif
+int draw(PlayerMap* players, BallMap* balls, Map* mainMap);
