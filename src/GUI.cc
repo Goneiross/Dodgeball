363f912e1af3de7e5359aef06aae11ee28107b97
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-04-12 10:35:10 +0200

    Revert "Update format"
    This reverts commit e06606a6baa4f89fac460a1d48196d968e973417.

--- src/GUI.cc
+++ src/GUI.cc
@@ -5,28 +5,28 @@
   \brief  "GUI" module implementation
 */
 
-#include "GUI.h"
-#include "define.h"
-#include "simulation.h"
-#include <glibmm/main.h>
 #include <gtkmm.h>
+#include <glibmm/main.h>
+#include "GUI.h"
 #include <iostream>
+#include "simulation.h"
+#include "define.h"
 
 #ifndef OBJECT_HEADER
 #define OBJECT_HEADER
+#include "player.h"
 #include "ball.h"
 #include "map.h"
-#include "player.h"
 #endif
 
 using namespace Gtk;
 
-class MyArea : public Gtk::DrawingArea {
+class MyArea: public Gtk::DrawingArea {
   public:
-    MyArea(PlayerMap* p, BallMap* b, Map* m) {
-        players = p;
-        balls = b;
-        mainMap = m;
+    MyArea(PlayerMap* p, BallMap* b, Map* m){
+      players = p;
+      balls = b;
+      mainMap = m;
     };
     virtual ~MyArea(){};
     void clear();
@@ -37,118 +37,111 @@ class MyArea : public Gtk::DrawingArea {
 
   protected:
     bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;
-
   private:
     bool empty;
     void refresh();
 };
 
-void MyArea::clear() {
-    empty = true;
-    refresh();
+void MyArea::clear(){
+  empty= true;
+  refresh();
 }
 
-void MyArea::draw() {
-    empty = false;
-    refresh();
+void MyArea::draw(){
+  empty= false;
+  refresh();
 }
 
-void MyArea::refresh() {
-    auto win = get_window();
-    if (win) {
-        Gdk::Rectangle r(0, 0, get_allocation().get_width(),
-                         get_allocation().get_height());
-        win->invalidate_rect(r, false);
-    }
+void MyArea::refresh(){
+  auto win = get_window();
+  if(win){
+    Gdk::Rectangle r(0,0, get_allocation().get_width(), get_allocation().get_height());
+    win->invalidate_rect(r,false);
+  }
 }
 
-bool MyArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr) {
-    if (not empty) {
-        Gtk::Allocation allocation = get_allocation();
-        int width = allocation.get_width();
-        int height = allocation.get_height();
-        int lesser = MIN(width, height);
+bool MyArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr){
+  if(not empty) {
+    Gtk::Allocation allocation = get_allocation();
+    int width = allocation.get_width();
+    int height = allocation.get_height();
+    int lesser = MIN(width, height);
 
-        int xc, yc;
-        xc = width / 2;
-        yc = height / 2;
-        int nbPlayer = players->getNb();
-        cr->save();
-        for (int p = 0; p < nbPlayer; p++) {
-            Player* player = players->getPlayer(p);
-            player->setGX(width / 2 + player->getX()); // PUT ELSEWHERE !
-            player->setGY(height / 2 - player->getY());
-            if (player->getTimeTouched() >= 4) { // Rename var //PUT IN A FUNCTION !!!
-                cr->set_source_rgba(0.0, 1, 0.0, 1);
-            } else if (player->getTimeTouched() == 3) {
-                cr->set_source_rgba(1, 1, 0.0, 1);
-            } else if (player->getTimeTouched() == 2) {
-                cr->set_source_rgba(1.0, 1, 0.0, 1);
-            } else {
-                cr->set_source_rgba(1.0, 0.0, 0.0, 1);
-            }
-            cr->arc(player->getGX(), player->getGY(),
-                    (player->getRadius() / SIDE) * lesser, 0.0, 2.0 * M_PI);
-            cr->fill_preserve();
-            cr->stroke();
-            cr->set_source_rgba(0.0, 0.0, 1.0, 1);
-            cr->arc(player->getGX(), player->getGY(),
-                    (player->getRadius() / SIDE) * lesser, 0.0,
-                    (player->getCount() / MAX_COUNT) * (2 * M_PI));
-            std::cout << (player->getCount() / MAX_COUNT) * (2 * M_PI) << std::endl;
-            cr->stroke();
-            player = nullptr;
-        }
-        cr->restore();
-
-        int nbBall = balls->getNb();
-        cr->save();
-        cr->set_source_rgba(0.0, 0.0, 1, 1);
-        for (int b = 0; b < nbBall; b++) {
-            Ball* ball = balls->getBall(b);
-
-            ball->setGX(width / 2 + ball->getX()); // PUT ELSEWHERE !
-            ball->setGY(height / 2 - ball->getY());
-
-            cr->arc(ball->getGX(), ball->getGY(), (ball->getRadius() / SIDE) * lesser,
-                    0.0, 2.0 * M_PI);
-            cr->fill_preserve();
-            cr->stroke();
-            ball = nullptr;
-        }
-        cr->restore();
-
-        int nbObstacle = mainMap->getNb();
-        cr->save();
+    int xc, yc;
+    xc = width / 2;
+    yc = height / 2;
+    int nbPlayer = players->getNb();
+    cr->save();
+    for (int p = 0; p < nbPlayer; p++){
+      Player* player = players->getPlayer(p);
+      player->setGX(width / 2 + player->getX()); //PUT ELSEWHERE !
+      player->setGY(height / 2 - player->getY());
+      if (player->getTimeTouched() >= 4){ //Rename var //PUT IN A FUNCTION !!!
+        cr->set_source_rgba(0.0, 1, 0.0, 1);
+      } else if (player->getTimeTouched() == 3){
+        cr->set_source_rgba(1, 1, 0.0, 1);
+      } else if (player->getTimeTouched() == 2){
+        cr->set_source_rgba(1.0, 1, 0.0, 1);
+      } else {
         cr->set_source_rgba(1.0, 0.0, 0.0, 1);
-        for (int o = 0; o < nbObstacle; o++) {
-            Obstacle* obstacle = mainMap->getObstacle(o);
-            double side = obstacle->getSide();
-
-            obstacle->setGX(width / 2 + obstacle->getX()); // PUT ELSEWHERE !
-            obstacle->setGY(height / 2 - obstacle->getY());
-            cr->move_to(obstacle->getGX() - side / 2, obstacle->getGY() - side / 2);
-            cr->rel_line_to(side, 0);
-            cr->rel_line_to(0, side);
-            cr->rel_line_to(-side, 0);
-            cr->rel_line_to(0, -side);
-            cr->fill_preserve();
-            cr->stroke();
-            obstacle = nullptr;
-        }
-        cr->restore();
-    } else {
-        std::cout << "Empty !" << std::endl;
+      }
+      cr->arc(player->getGX(), player->getGY(), (player->getRadius() / SIDE) * lesser, 0.0, 2.0 * M_PI);
+      cr->fill_preserve();
+      cr->stroke();
+      cr->set_source_rgba(0.0, 0.0, 1.0, 1);
+      cr->arc(player->getGX(), player->getGY(), (player->getRadius() / SIDE) * lesser, 0.0, (player->getCount() / MAX_COUNT) * (2 * M_PI));
+      std::cout << (player->getCount() / MAX_COUNT) * (2 * M_PI) << std::endl;
+      cr->stroke();
+      player = nullptr;
     }
-    return true;
+    cr->restore();
+
+
+    int nbBall = balls->getNb();
+    cr->save();
+    cr->set_source_rgba(0.0, 0.0, 1, 1);
+    for (int b = 0; b < nbBall; b++){
+      Ball* ball = balls->getBall(b);
+
+      ball->setGX(width / 2 + ball->getX()); //PUT ELSEWHERE !
+      ball->setGY(height / 2 - ball->getY());
+      
+      cr->arc(ball->getGX(), ball->getGY(), (ball->getRadius() / SIDE) * lesser, 0.0, 2.0 * M_PI);
+      cr->fill_preserve();
+      cr->stroke();
+      ball = nullptr;
+    }
+    cr->restore();
+
+    int nbObstacle = mainMap->getNb();
+    cr->save();
+    cr->set_source_rgba(1.0, 0.0, 0.0, 1);
+    for (int o = 0; o < nbObstacle; o++){
+      Obstacle* obstacle = mainMap->getObstacle(o);
+      double side = obstacle->getSide();
+
+      obstacle->setGX(width / 2 + obstacle->getX()); //PUT ELSEWHERE !
+      obstacle->setGY(height / 2 - obstacle->getY());
+      cr->move_to(obstacle->getGX() - side / 2 , obstacle->getGY() - side / 2);
+      cr->rel_line_to(side, 0);
+      cr->rel_line_to(0, side);
+      cr->rel_line_to(-side, 0);
+      cr->rel_line_to(0, - side);
+      cr->fill_preserve();
+      cr->stroke();
+      obstacle = nullptr;
+    }
+    cr->restore();
+  } else { std::cout << "Empty !" << std::endl; }
+  return true;
 }
 
-class GUI : public Window {
+class GUI: public Window {
   public:
     GUI(PlayerMap* players, BallMap* balls, Map* mainMap);
-    virtual ~GUI();
-
-  protected: // Or private ?
+    virtual ~GUI(); 
+    
+  protected: //Or private ?
     void on_button_clicked_exit();
     void on_button_clicked_open();
     void on_button_clicked_save();
@@ -169,148 +162,158 @@ class GUI : public Window {
     MyArea m_area;
 };
 
-GUI::GUI(PlayerMap* p, BallMap* b, Map* m)
-    : m_box_top(Gtk::ORIENTATION_VERTICAL), m_box1(Gtk::ORIENTATION_HORIZONTAL, 10),
-      m_box2(Gtk::ORIENTATION_HORIZONTAL, 10), m_button_exit("Exit"),
-      m_button_open("Open"), m_button_save("Save"), m_button_start("Start"),
-      m_button_step("Step"), m_label_status("No game to run"), m_area(p, b, m),
-      timer_added(false), disconnect(false), timeoutValue(DELTA_T * 1000) {
+GUI::GUI(PlayerMap* p, BallMap* b, Map* m): 
+  m_box_top(Gtk::ORIENTATION_VERTICAL),
+  m_box1(Gtk::ORIENTATION_HORIZONTAL, 10),
+  m_box2(Gtk::ORIENTATION_HORIZONTAL, 10),
+  m_button_exit("Exit"),
+  m_button_open("Open"),
+  m_button_save("Save"),
+  m_button_start("Start"),
+  m_button_step("Step"),
+  m_label_status("No game to run"),
+  m_area(p, b, m),
+  timer_added(false),
+  disconnect(false),
+  timeoutValue(DELTA_T * 1000){
+  
+  set_title("DodgeBall");
+  set_border_width(0);
 
-    set_title("DodgeBall");
-    set_border_width(0);
+  m_box1.set_size_request(10, 10);
 
-    m_box1.set_size_request(10, 10);
+  add(m_box_top);
+  m_box_top.pack_start(m_box1, false, false);
+  m_box_top.pack_start(m_box2);
 
-    add(m_box_top);
-    m_box_top.pack_start(m_box1, false, false);
-    m_box_top.pack_start(m_box2);
+  m_area.set_size_request(SIDE,SIDE);
+  m_box2.pack_start(m_area);
 
-    m_area.set_size_request(SIDE, SIDE);
-    m_box2.pack_start(m_area);
+  m_box1.pack_start(m_button_exit, false, false);
+  m_box1.pack_start(m_button_open, false, false);
+  m_box1.pack_start(m_button_save, false, false);
+  m_box1.pack_start(m_button_start, false, false);
+  m_box1.pack_start(m_button_step, false, false);
+  m_box1.pack_start(m_label_status, false, false);
 
-    m_box1.pack_start(m_button_exit, false, false);
-    m_box1.pack_start(m_button_open, false, false);
-    m_box1.pack_start(m_button_save, false, false);
-    m_box1.pack_start(m_button_start, false, false);
-    m_box1.pack_start(m_button_step, false, false);
-    m_box1.pack_start(m_label_status, false, false);
+  m_button_exit.signal_clicked().connect(sigc::mem_fun(*this,&GUI::on_button_clicked_exit));
+  m_button_open.signal_clicked().connect(sigc::mem_fun(*this,&GUI::on_button_clicked_open));
+  m_button_save.signal_clicked().connect(sigc::mem_fun(*this,&GUI::on_button_clicked_save));
+  m_button_start.signal_clicked().connect(sigc::mem_fun(*this,&GUI::on_button_clicked_start));
+  m_button_step.signal_clicked().connect(sigc::mem_fun(*this,&GUI::on_button_clicked_step));
+   
+  show_all_children();
 
-    m_button_exit.signal_clicked().connect(
-        sigc::mem_fun(*this, &GUI::on_button_clicked_exit));
-    m_button_open.signal_clicked().connect(
-        sigc::mem_fun(*this, &GUI::on_button_clicked_open));
-    m_button_save.signal_clicked().connect(
-        sigc::mem_fun(*this, &GUI::on_button_clicked_save));
-    m_button_start.signal_clicked().connect(
-        sigc::mem_fun(*this, &GUI::on_button_clicked_start));
-    m_button_step.signal_clicked().connect(
-        sigc::mem_fun(*this, &GUI::on_button_clicked_step));
-
-    show_all_children();
-
-    maximize();
+  maximize();
 }
 
-GUI::~GUI() {}
+GUI::~GUI(){}
 
-void GUI::on_button_clicked_exit() { hide(); }
+void GUI::on_button_clicked_exit(){ hide(); }
 
-void GUI::on_button_clicked_open() {
-    Gtk::FileChooserDialog dialog("Please choose a file",
-                                  Gtk::FILE_CHOOSER_ACTION_OPEN);
-    dialog.set_transient_for(*this);
-    dialog.add_button("_Cancel", Gtk::RESPONSE_CANCEL);
-    dialog.add_button("_Open", Gtk::RESPONSE_ACCEPT);
-    int result = dialog.run();
-    switch (result) {
-    case (Gtk::RESPONSE_ACCEPT): {
-        std::string inputFile = dialog.get_filename();
-        bool success =
-            initialization(inputFile, m_area.players, m_area.mainMap, m_area.balls);
-        if (success) {
-            m_area.clear();
-            m_area.draw();
-            m_label_status.set_label("Game ready to run");
-        } else {
-            m_area.clear();
-            m_label_status.set_label("No game to run");
-        }
-        break;
+void GUI::on_button_clicked_open(){
+  Gtk::FileChooserDialog dialog("Please choose a file",
+          Gtk::FILE_CHOOSER_ACTION_OPEN);
+  dialog.set_transient_for(*this);
+  dialog.add_button("_Cancel", Gtk::RESPONSE_CANCEL);
+  dialog.add_button("_Open", Gtk::RESPONSE_ACCEPT);
+  int result = dialog.run();
+  switch(result) {
+    case(Gtk::RESPONSE_ACCEPT): {
+      std::string inputFile = dialog.get_filename();
+      bool success = initialization(inputFile, m_area.players, m_area.mainMap, m_area.balls);
+      if (success) {
+        m_area.clear();
+        m_area.draw();
+        m_label_status.set_label("Game ready to run");
+      } else {
+        m_area.clear();
+        m_label_status.set_label("No game to run");
+      }
+      break;
     }
-    case (Gtk::RESPONSE_CANCEL): {
-        break;
+    case(Gtk::RESPONSE_CANCEL): {
+      break;
     }
-    default: { break; }
+    default: {
+      break;
     }
+  }
 }
 
-void GUI::on_button_clicked_save() {
-    Gtk::FileChooserDialog dialog("Please choose a file",
-                                  Gtk::FILE_CHOOSER_ACTION_SAVE);
-    dialog.set_transient_for(*this);
-    dialog.add_button("_Cancel", Gtk::RESPONSE_CANCEL);
-    dialog.add_button("_Save", Gtk::RESPONSE_ACCEPT);
-    int result = dialog.run();
-    switch (result) {
-    case (Gtk::RESPONSE_ACCEPT): {
-        std::string filename = dialog.get_filename();
-        save(filename, m_area.mainMap->getLNb(), m_area.players, m_area.mainMap,
-             m_area.balls);
-        break;
+void GUI::on_button_clicked_save(){
+  Gtk::FileChooserDialog dialog("Please choose a file",
+          Gtk::FILE_CHOOSER_ACTION_SAVE);
+  dialog.set_transient_for(*this);
+  dialog.add_button("_Cancel", Gtk::RESPONSE_CANCEL);
+  dialog.add_button("_Save", Gtk::RESPONSE_ACCEPT);
+  int result = dialog.run();
+  switch(result) {
+    case(Gtk::RESPONSE_ACCEPT): {
+      std::string filename = dialog.get_filename();
+      save(filename, m_area.mainMap->getLNb(), m_area.players, m_area.mainMap, m_area.balls);
+      break;
     }
-    case (Gtk::RESPONSE_CANCEL): {
-        break;
+    case(Gtk::RESPONSE_CANCEL): {
+      break;
     }
-    default: { break; }
+    default: {
+      break;
     }
+  }
 }
 
-void GUI::on_button_clicked_start() {
-    if (not timer_added) {
-        Glib::signal_timeout().connect(sigc::mem_fun(*this, &GUI::on_timeout),
-                                       timeoutValue);
+void GUI::on_button_clicked_start(){ 
+  if(not timer_added)
+  {	  
+	  Glib::signal_timeout().connect( sigc::mem_fun(*this, &GUI::on_timeout),
+									  timeoutValue );
+		
+	  timer_added = true;
+    m_button_start.set_label("Stop");
+  } else {
+    disconnect  = true;   
+    timer_added = false;
+    m_button_start.set_label("Start");
+  }
+ }
 
-        timer_added = true;
-        m_button_start.set_label("Stop");
-    } else {
-        disconnect = true;
-        timer_added = false;
-        m_button_start.set_label("Start");
-    }
-}
-
-void GUI::on_button_clicked_step() {
+void GUI::on_button_clicked_step(){ 
+  update(m_area.balls, m_area.players);
+  check(m_area.balls, m_area.players);
+  auto win = get_window();
+  if (win)
+  {
+      Gdk::Rectangle r(0, 0, get_allocation().get_width(),
+                        get_allocation().get_height());
+      win->invalidate_rect(r, false);
+  }
+ }
+bool GUI::on_timeout()
+{
+  if(disconnect)
+  {
+	  disconnect = false;
+	  
+	  return false;
+  } else {
     update(m_area.balls, m_area.players);
     check(m_area.balls, m_area.players);
     auto win = get_window();
-    if (win) {
+    if (win)
+    {
         Gdk::Rectangle r(0, 0, get_allocation().get_width(),
-                         get_allocation().get_height());
+                get_allocation().get_height());
         win->invalidate_rect(r, false);
     }
-}
-bool GUI::on_timeout() {
-    if (disconnect) {
-        disconnect = false;
-
-        return false;
-    } else {
-        update(m_area.balls, m_area.players);
-        check(m_area.balls, m_area.players);
-        auto win = get_window();
-        if (win) {
-            Gdk::Rectangle r(0, 0, get_allocation().get_width(),
-                             get_allocation().get_height());
-            win->invalidate_rect(r, false);
-        }
-        return true;
-    }
+    return true;
+  }
 }
 
-int draw(PlayerMap* players, BallMap* balls, Map* mainMap) {
+int draw(PlayerMap* players, BallMap* balls, Map* mainMap){
     auto app = Application::create();
 
     GUI mainWindow(players, balls, mainMap);
     app->run(mainWindow);
-    return 0;
+  return 0;
 }
--- src/ball.cc
+++ src/ball.cc
@@ -4,10 +4,10 @@
   \date   March 2019
   \brief  "Ball" module implementation
 */
-#include "ball.h"
-#include "define.h"
-#include <math.h>
 #include <vector>
+#include <math.h>
+#include "define.h"
+#include "ball.h"
 #ifndef TOOLS_H
 #define TOOLS_H
 #include "tools.h"
@@ -16,15 +16,14 @@
 using namespace std;
 
 Ball::Ball(double x, double y, double a, double r, double v, int l, int c, int i) {
-
-    angle = a;
-    ID = i;
-    velocity = v;
-    lPosition = l;
-    cPosition = c;
-    gXPosition = -1;
-    gYPosition = -1;
-    hitbox = new Circle(x, y, r);
+  angle = a;
+  ID = i;
+  velocity = v;
+  lPosition = l;
+  cPosition = c;
+  gXPosition = -1;
+  gYPosition = -1;
+  hitbox = new Circle(x, y, r);
 }
 
 double Ball::getX() const { return hitbox->getX(); }
@@ -36,83 +35,85 @@ double Ball::getGY() const { return gYPosition; }
 void Ball::setGX(double gX) { gXPosition = gX; }
 void Ball::setGY(double gY) { gYPosition = gY; }
 double Ball::getRadius() const { return hitbox->getRadius(); }
-Circle* Ball::getHitbox() const { return hitbox; }
-double Ball::getAngle() const { return angle; }
-double Ball::getVelocity() const { return velocity; }
+Circle *Ball::getHitbox() const { return hitbox; }
+double Ball::getAngle() const {return angle; }
+double Ball::getVelocity() const {return velocity; }
 
-BallMap::BallMap(int l, int c) {
-    lineNumber = l;
-    columnNumber = c;
+BallMap::BallMap(int l, int c){
+  lineNumber = l;
+  columnNumber = c;
 
-    ballGrid = new vector<int>*[lineNumber];
-    for (int i = 0; i < lineNumber; i++) {
-        ballGrid[i] = new vector<int>[columnNumber];
-        for (int j = 0; j < columnNumber; j++) {
-            ballGrid[i][j].push_back(-1);
-        }
+  ballGrid = new vector<int>* [lineNumber];
+  for (int i = 0; i < lineNumber; i++) {
+    ballGrid[i] = new vector<int>[columnNumber];
+    for (int j = 0; j < columnNumber; j++) {
+      ballGrid[i][j].push_back(-1);
     }
+  }
 }
 
-void BallMap::addBall(double x, double y, double a, double r, double v, int ID) {
-    int cPosition = ((x + DIM_MAX) / (SIDE / lineNumber)) - 1 / 2;
-    int lPosition = -((y - DIM_MAX) / (SIDE / lineNumber)) - 1 / 2;
-    balls.push_back(new Ball(x, y, a, r, v, cPosition, lPosition, ID));
-    if (ballGrid[lPosition][cPosition][0] == -1) {
-        ballGrid[lPosition][cPosition][0] = ID;
-    } else {
-        ballGrid[lPosition][cPosition].push_back(ID);
-    }
+void BallMap::addBall(double x, double y, double a, double r, double v, int ID){
+  int cPosition = ((x + DIM_MAX ) / (SIDE / lineNumber) ) - 1 / 2;
+  int lPosition = - ((y - DIM_MAX) / (SIDE / lineNumber) ) - 1 / 2;
+  balls.push_back(new Ball(x, y, a, r, v, cPosition, lPosition, ID));
+  if (ballGrid[lPosition][cPosition][0] == -1){
+    ballGrid[lPosition][cPosition][0] = ID;
+  } else {
+    ballGrid[lPosition][cPosition].push_back(ID);
+  }
 }
-void BallMap::removeBall(int ID) {
-    int l = balls[ID]->getL();
-    int c = balls[ID]->getC();
-    int posSize = ballGrid[l][c].size();
-    if (ballGrid[l][c][0] == ID) {
-        ballGrid[l][c][0] = -1;
-    } else {
-        for (int i = 1; i < posSize; i++) {
-            if (ballGrid[l][c][i] == ID) {
-                ballGrid[l][c].erase(ballGrid[l][c].begin() + i);
-            }
-        }
+void BallMap::removeBall(int ID){
+  int l = balls[ID]->getL();
+  int c = balls[ID]->getC();
+  int posSize = ballGrid[l][c].size();
+  if (ballGrid[l][c][0] == ID){
+      ballGrid[l][c][0] = -1;
+  } else {
+    for (int i = 1; i < posSize; i++){
+      if (ballGrid[l][c][i] == ID){
+        ballGrid[l][c].erase(ballGrid[l][c].begin()+i);
+      }
     }
-    balls.erase(balls.begin() + ID);
-}
-
-void BallMap::removeAll() {
-    int ballNb = balls.size();
-    for (int i = 0; i < ballNb; i++) {
-        removeBall(0);
-    }
+  }
+  balls.erase(balls.begin()+ ID );
 }
 
-bool BallMap::isBall(int lPosition, int cPosition) {
-    if (ballGrid[lPosition][cPosition][0] == -1) {
-        return false;
-    } else {
-        return true;
-    }
+void BallMap::removeAll(){
+  int ballNb = balls.size();
+  for (int i = 0; i < ballNb; i++){
+    removeBall(0);
+  }
 }
 
-vector<int> BallMap::whichBall(int lPosition, int cPosition) {
-    return ballGrid[lPosition][cPosition];
+bool BallMap::isBall(int lPosition, int cPosition){
+  if (ballGrid[lPosition][cPosition][0] == -1){
+    return false;
+  } else {
+    return true;
+  }
 }
 
-void BallMap::reserveSpace(int nbBall) { balls.reserve(nbBall); }
+vector<int> BallMap::whichBall(int lPosition, int cPosition){
+  return ballGrid[lPosition][cPosition];
+}
 
-Ball* BallMap::getBall(int i) const { return balls[i]; }
+void BallMap::reserveSpace(int nbBall){
+  balls.reserve(nbBall);
+}
 
-int BallMap::getNb() const { return balls.size(); }
+Ball* BallMap::getBall(int i) const {
+  return balls[i];
+}
 
-void BallMap::updatePosition() {
-    int ballNb = balls.size();
-    for (int b = 0; b < ballNb; b++) {
-        balls[b]->getHitbox()->setX(balls[b]->getHitbox()->getX() +
-                                    cos(balls[b]->getAngle()) *
-                                        balls[b]->getVelocity());
-        balls[b]->getHitbox()->setY(balls[b]->getHitbox()->getY() +
-                                    sin(balls[b]->getAngle()) *
-                                        balls[b]->getVelocity());
-        // Compute new Line and Column
-    }
+int BallMap::getNb() const{
+  return balls.size();
+}
+
+void BallMap::updatePosition(){
+  int ballNb = balls.size();
+  for (int b = 0; b < ballNb; b++){
+    balls[b]->getHitbox()->setX(balls[b]->getHitbox()->getX() + cos(balls[b]->getAngle()) * balls[b]->getVelocity());
+    balls[b]->getHitbox()->setY(balls[b]->getHitbox()->getY() + sin(balls[b]->getAngle()) * balls[b]->getVelocity());
+    // Compute new Line and Column
+  }
 }
