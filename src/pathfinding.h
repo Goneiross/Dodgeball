d9f37f35aa0028bd53d5ea071c6cc7be9e8949a0
einsetumadur <gillesoff@gmail.com>
2019-04-13 10:04:48 +0200

    Add small functions for the pathfinding algoritm

--- src/pathfinding.h
+++ src/pathfinding.h
@@ -16,11 +16,15 @@
 #include "tools.h"
 #endif
 
+typedef tile int[2];
 
 double pathAngle (PlayerMap* players, int index, Map* obstacles );
 int nearestPlayer (PlayerMap* players, int index);
 bool openGridArea (bool[][] openGrid, bool[][] closedGrid, int l,int c);
-void setupClosedGrid(bool[][] closedGrid , Map* mainMap);
+void setupOpenGrid(bool[][] openGrid , Map* mainMap);
+tile lowestScoreTile(double[][] scoreGrid);
+bool isInGrid(double[][] scoreGrid,int l, int c);
 
+double tileScore(Player* players, int index, int enemyIndex, bool[][] openGrid)
 double distanceCost(PlayerMap* players, int index);
 double distanceApprox(PlayerMap* players, int enemyIndex);
