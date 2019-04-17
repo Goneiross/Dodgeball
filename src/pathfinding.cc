68430d52d15bb5ba0dd4afb5414c16003aa6e25f
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-04-17 11:26:01 +0200

    Fix some syntax

--- src/pathfinding.cc
+++ src/pathfinding.cc
@@ -20,60 +20,87 @@
 #include "tools.h"
 #endif
 
-typedef int[2] tile;
+#include <iostream>
+
+using namespace std;
+
+typedef struct Tile {
+  int l;
+  int c;
+}
+Tile;
 
 class Path {
 public:
-  Path(int nbCell){
-    path_[nbCell*nbCell][3] = {-1};
-  }
-  int getParent(int child);
-  tile getTile(int tileIndex);
-
+    Path(int nbCell);
+    int getParent(int child);
+    Tile getTile(int TileIndex);
 private:
-//voulais: int[taille map n^2][], pas sûr de mon code.
-//logique: path[n]=>(parent,coord l,coord c)
-  int[][] path_;
+    int** way;
 };
 
+
+Path::Path(int nbCell) {
+    int size = nbCell * nbCell;
+    way = new int*[size];
+    for (int i = 0; i < size; i++){
+        way[i] = new int[3];
+        for (int j = 0; j < 3; j++){
+            way[i][j] = -1;
+        }
+    }
+}
+
 int nearestPlayer (PlayerMap* players, int index);
-bool openGridArea (bool[][] openGrid, bool[][] closedGrid, int l,int c);
-void setupOpenGrid(bool[][] openGrid , Map* mainMap);
-tile lowestScoreTile(double[][] scoreGrid);
-bool isInGrid(double[][] scoreGrid,int l, int c);
-double tileScore(Player* players, int index, int enemyIndex, bool[][] openGrid)
+bool openGridArea (bool** openGrid, bool** closedGrid, int l,int c);
+void setupOpenGrid(bool** openGrid , Map* mainMap);
+Tile lowestScoreTile(double** scoreGrid);
+bool isInGrid(double** scoreGrid,int l, int c);
+double TileScore(Player* players, int index, int enemyIndex, bool** openGrid);
 double distanceCost(PlayerMap* players, int index);
 double distanceApprox(PlayerMap* players, int enemyIndex);
 
-double pathAngle (PlayerMap* players, int index, Map* obstacles )
-{
-  double angle = 0;
-  int nearest = nearestPlayer(players,index);
-  double scoreGrid[players->getLNb()][players->getCNb()]
-    = {players->getLNb()*sqrt(2)};
-  bool openGrid[players->getLNb()][players->getCNb()] = {true};
-  tile me;
-  me[0] = players->getPlayer(index)->getL();
-  me[1] = players->getPlayer(index)->getC();
-  tile enemy;
-  enemy[0] = players->getPlayer(nearest)->getL();
-  enemy[1] = players->getPlayer(nearest)->getC();
-  tile current;
-  current[0] = me[0];
-  current[1] = me[1];
-  tile last;
+double pathAngle (PlayerMap* players, int index, Map* obstacles ) {
+    double angle = 0;
+    int nearest = nearestPlayer(players,index);
+    int LNb = players->getLNb(), CNb = players->getCNb();
+    double** scoreGrid = new double*[LNb]; //call LNb and CNb outside !
+    for (int i = 0; i < LNb; i++){
+        scoreGrid[i] = new double[CNb];
+        for (int j = 0; j < CNb; j++){
+            scoreGrid[i][j] = LNb*sqrt(2); //Compute outside !
+        }
+    }
+    bool** openGrid = new bool*[LNb];
+    for (int i = 0; i < LNb; i++){
+        openGrid[i] = new bool[CNb];
+        for (int j = 0; j < CNb; j++){
+            openGrid[i][j] = true;
+        }
+    }
 
-  setupOpenGrid(openGrid,obstacles);
+    Tile me;
+    me.l = players->getPlayer(index)->getL();
+    me.c = players->getPlayer(index)->getC();
+    Tile enemy;
+    enemy.l = players->getPlayer(nearest)->getL();
+    enemy.c = players->getPlayer(nearest)->getC();
+    Tile current;
+    current.l = me.l;
+    current.c = me.c;
+    Tile last;
 
-  do{                                       // A* algorithm under construction
+    setupOpenGrid(openGrid,obstacles);
 
-  }while(!openGrid.empty())
+    do {                                       // A* algorithm under construction
+        std::cout << "Hello" << std::endl;
+        break;
+    } while(!openGrid.empty());
 
-  return angle;
+    return angle;
 }
 
-int nearestPlayer (PlayerMap* players, int index)
-{
+int nearestPlayer (PlayerMap* players, int index) {
   int nearest(-1);
   double distance = players->getLNb()*sqrt(2);
 
@@ -91,25 +118,24 @@ int nearestPlayer (PlayerMap* players, int index)
   return nearest;
 }
 
-tile lowestScoreTile(double[][] scoreGrid, tile lastTile)
-{
+Tile lowestScoreTile(double** scoreGrid, Tile lastTile) {
   int gridSize = scoreGrid.size();
   double lowestScore = scoreGrid*sqrt(2);
-  tile lowest = {-1,-1};
+  Tile lowest = {-1,-1};
   double value;
   for (int radius = 1; radius < gridSize; radius++) {
     for (int i = 0; i < (2*radius)-1 ; i++) {
       if(isInGrid(scoreGrid,l+radius,i)){       //sensé éviter le seg fault.
         value = scoreGrid[l+radius][i];
         if(value < lowestScore){
-          lowest[0] = l+radius;
-          lowest[1] = i;
+          lowest.l = l+radius;
+          lowest.c = i;
         }
       if(isInGrid(scoreGrid,i,l+radius)){       //sensé éviter le seg fault.
         value = scoreGrid[i][l+radius];
         if(value < lowestScore){
-          lowest[0] = i;
-          lowest[1] = l+radius;
+          lowest.l = i;
+          lowest.c = l+radius;
         }
       }
     }
@@ -117,8 +143,7 @@ tile lowestScoreTile(double[][] scoreGrid, tile lastTile)
   return lowest;
 }
 
-bool isInGrid(double[][] scoreGrid,int l, int c)
-{
+bool isInGrid(double** scoreGrid,int l, int c) {
   int height = scoreGrid.size();
   int width = scoreGrid[].size();
   if((l < heigt) and (c < width)){
@@ -127,12 +152,11 @@ bool isInGrid(double[][] scoreGrid,int l, int c)
   return false;
 }
 
-bool openGridArea (bool[][] openGrid, bool[][] closedGrid, int l,int c)
-{
+bool openGridArea (bool** openGrid, bool** closedGrid, int l,int c) {
 
 }
-void setupOpenGrid(bool[][] openGrid , Map* mainMap)
-{
+
+void setupOpenGrid(bool** openGrid , Map* mainMap) {
   for(auto line : openGrid){
     for(auto column : line){
       if(mainMap->isObstacle(line,column)){
