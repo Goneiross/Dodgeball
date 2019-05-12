a9eb37c71ed5961122c2d89ef4ba72f2f5a9b2f2
Quelqunquiteveutdubien <gllmmch77@gmail.com>
2019-05-12 13:29:18 +0200

    completed comments, solved mathematical questions, pathAngle progressed, added comments

--- src/floyd.cc
+++ src/floyd.cc
@@ -3,8 +3,6 @@
   \author Guillaume Pellerin & Vincent Miche
   \date   May 2019
   \brief  pathfinding, with the Roy-Floyd-Warschall algorithm
-  to check the size of each line (<=87 character): delete this line:
-123456789012345678901234567890123456789012345678901234567890123456789012345678901234567
 */
 
 #include <iostream>
@@ -85,7 +83,11 @@ double floyd() {
 	 * l'angle dans lequel start doit partir
 	 * Plus tard, shortestindirectDist cherchera mieux
 	 * IL FAUT REUSSIR A SPECIFIER QUE pathAngle = angle entre start et la première
-	 * position à atteindre*/
+	 * position à atteindre
+	 * Faut-il que tabCellDist soit static ou pas ?
+	 * S'il l'est, ne faudrait-il pas faire un tabPathAngle plutôt qu'un pathAngle unique ?
+	 * 	car si on enregistre uniquement un pathAngle, alors comment fait-on pour connaitre
+	 * 	celui d'un autre chemin si on ne recalcule pas le meilleur chemin pour tout ?*/
 	
 	//initialisation d'un tableau de distances
 	vector <vector<double> > tabCellDist (nbCell**2);
@@ -122,7 +124,7 @@ double floyd() {
 }
 
 //on essaie de trouver de meilleures distances
-	for (int i(0); i < nbCell**2; i++) {			 
+	for (int i(0); i < pow(nbCell, 2); i++) {			 
 			nbFloyd = INFINITY_DIST;
 			do {
 				if (tabCellDist[i][j] = INFINITY_INIT) {
@@ -134,30 +136,35 @@ double floyd() {
 }
 
 void shortestIndirectDist() {
-	for (int k(0); k < nbCell**2; k++) {
-//		if ((tabCellDist[i][k] < nbCell**2) && (tabCellDist[k][j] < nbCell**2)) {
+	for (int k(0); k < pow(nbCell, 2); k++) {
+		if ((tabCellDist[i][k] < pow(nbCell, 2)) && (tabCellDist[k][j] < pow(nbCell, 2))) {
 				int tmpDist(distance(i, k) + distance(k, j));
 				if (tmpDist < tabCellDiste[i][j]) {
 					tabCellDist[i][j] = tmpDist;
 					//pathAngle = l'angle entre i et k;
+					double iXPos = i/nbCell;
+					double iYPos = i%nbCell;
+					double kXPos = k/nbCell;
+					double kYPos = k%nbCell;
+					double pathAngle = atan(vabs(kYPos-iYPos)/vabs(kXPos-iXPos));
 			}
-//		}
+		}
 	}
+	return pathAngle;
 }
 
 void diagonalDistance(unsigned int i, unsigned int j) {
-	double iXPos = i%nbCell;
-	double iYPos = i/nbCell;
-	double jXPos = 
-	/* 	avec 	1x le 	x de la coordonnée correspondant à la case 	i
-	 * 			1y		y											i
-	 * 			2x		x											j
-	 * 			2y		y											j
-	 * if (isObstacle(1x, 2y) xor isObstacle(2x, 1y)) {
-	 * 	 		tabCellDist[i][j] = 2;
-	 * 			tabCellDist[j][i] = 2;
-	 * 	} else if (!(isObstacle(1x, 2y) || isObstacle(2x, 1y))) {
-	 * 		tabCellDist[i][j] = sqrt(2);
-	 * 		tabCellDist[j][i] = sqrt(2);
-	 * 	}*/
+	double iXPos = i/nbCell;
+	double iYPos = i%nbCell;
+	double jXPos = j/nbCell;
+	double jYPos = j%nbCell;
+	//si un Obstacle en diagonale
+  if (isObstacle(iXPos, jYPos) xor isObstacle(jXPos, iYPos)) {
+		tabCellDist[i][j] = 2;
+		tabCellDist[j][i] = 2;
+	//si aucun Obstacle en diagonale
+	} else if (!(isObstacle(iXPos, jYPos)) && !(isObstacle(jXPos, iYPos))) {
+		tabCellDist[i][j] = sqrt(2);
+		tabCellDist[j][i] = sqrt(2);
+	} //si 2 Obstacles, il faut chercher mieux, donc on laisse à INFINITY_INIT
 }
