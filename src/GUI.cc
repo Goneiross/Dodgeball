72e25415d1a25527d563e0bf5a356ee9168516b9
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-05-13 20:32:13 +0200

    Add dracarys to GUI

--- src/GUI.cc
+++ src/GUI.cc
@@ -250,6 +250,7 @@ void GUI::on_button_clicked_start(){
 void GUI::on_button_clicked_step(){ 
   	update();
   	check();
+		dracarys();
   	auto win = get_window();
   	if (win) {
       	Gdk::Rectangle r(0, 0, get_allocation().get_width(),
@@ -265,6 +266,7 @@ bool GUI::on_timeout() {
   	} else {
     	update();
     	check();
+			dracarys();
     	auto win = get_window();
     	if (win) {
       	  	Gdk::Rectangle r(0, 0, get_allocation().get_width(),
