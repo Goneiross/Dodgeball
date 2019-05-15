7f85b85eb4e17703f32798969e01f8e3cc60ff2c
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-05-15 14:38:23 +0200

    Small fixes

--- src/pathfinding.cc
+++ src/pathfinding.cc
@@ -5,17 +5,17 @@
 using namespace std;
 
 static vector<int> targets;
-static vector<int> pathAngle;
+static vector<int> pathAngles;
 
 void pathfinding(PlayerMap* players, ObstacleMap* obstacles){
-    targets.resize(players.getNb())
-    pathAngle.resize(players.getNb());
+    targets.resize(players.getNb());
+    pathAngles.resize(players.getNb());
     targets = targetting(players);
     for (int p = 0; p < players.getNb(); p++){
-        pathAngle[p] = whichPath(players->getPlayer(p), players->getPlayer(targets[p])); 
+        pathAngles[p] = whichPath(players->getPlayer(p), players->getPlayer(targets[p]), obstacles); 
     }
 }
 
 double nearestPlayerAngle(int ID){
-    return pathAngle[ID];
+    return pathAngles[ID];
 }
