ff844b3274fe2fa0b3eb2e54061b6c762ca1eee0
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-05-17 10:17:58 +0200

    Now compute only one time floyd Some more debugging

--- src/floyd.cc
+++ src/floyd.cc
@@ -15,22 +15,25 @@
 
 using namespace std;
 
+	static vector <vector<double> > tabCellDist;
+	static vector <vector<double> > pathAngles;
+
 bool isThereObstacleBetween(int l1, int c1, int l2, int c2, ObstacleMap* obstacles);
 double simplePath(Player* start, Player* target);
-void complexPath(Player* start, Player* target, int infinityInit, int infinityDist);
-double floyd(Player* start, Player* target, int infinityInit, int infinityDist);
+void complexPath(Player* start, Player* target, int infinityInit, int infinityDist, bool &firstInStep);
+void floyd(Player* start, Player* target, int infinityInit, int infinityDist);
 void diagonalDistance(unsigned int i, unsigned int j, vector<vector<double> > &tabCellDist, vector<vector<double> > &pathAngles, ObstacleMap* obstacles);
 bool areThereUninitialisedCases (vector <vector<double> > tab2D, int infinityInit);
 void shortestIndirectPath(int startID, int targetID, vector<vector<double> > &tabCellDist, vector<vector<double> > &pathAngles, int infinityDist, int nbCell);
 
 vector<int> targetting(PlayerMap* players, int infinityInit, int infinityDist){
-	cout << "infinityInit: " << infinityInit << endl;
+	// cout << "infinityInit: " << infinityInit << endl;
 	vector<vector<double> > dBP(players->getNb(), vector<double>(players->getNb(), infinityInit)); //dBP = distance between players
 	for (int i=0; i < players->getNb(); i++) {
 		for (int j=0; j < i; j++) {
 			dBP[i][j] = distance(players->getPlayer(i)->getL(), players->getPlayer(i)->getC(), players->getPlayer(j)->getL(), players->getPlayer(j)->getC()); //L C or C L
 			dBP[j][i] = dBP[i][j];
-			cout << "start ID: " << i << " target ID:" << j << " distance:" << distance(players->getPlayer(i)->getL(), players->getPlayer(i)->getC(), players->getPlayer(j)->getL(), players->getPlayer(j)->getC()) << endl;
+			cout << "start ID: " << i << "(" << players->getPlayer(i)->getL() << "," << players->getPlayer(i)->getC() << ") target ID: " << j << "(" << players->getPlayer(j)->getL() << "," << players->getPlayer(j)->getC() << ") distance:" << distance(players->getPlayer(i)->getL(), players->getPlayer(i)->getC(), players->getPlayer(j)->getL(), players->getPlayer(j)->getC()) << endl;
 
 		}
 	}
@@ -110,12 +113,12 @@ double simplePath(Player* start, Player* target) {
 	return angle(start->getL(), start->getC(), target->getL(), target->getC());
 }
 
-double floyd(Player* start, Player* target, int infinityInit, int infinityDist, ObstacleMap* obstacles) {
+void floyd(Player* start, Player* target, int infinityInit, int infinityDist, ObstacleMap* obstacles) {
 	int nbCell = obstacles->getLNb();
 	int nbCellSquared = pow(nbCell, 2);
 	//initialisation d'un tableau de distances
-	static vector <vector<double> > tabCellDist (nbCellSquared, vector<double>(nbCellSquared, infinityInit));
-	static vector <vector<double> > pathAngles (nbCellSquared, vector<double>(nbCellSquared, 0));
+			tabCellDist.resize(nbCellSquared, vector<double>(nbCellSquared, infinityInit));
+			pathAngles.resize(nbCellSquared, vector<double>(nbCellSquared, 0));
 	for (int i(0); i < nbCellSquared; i++) {
     	for (int j(0); j < nbCellSquared; j++) {
     	tabCellDist[i][j] = infinityInit;
@@ -149,30 +152,30 @@ double floyd(Player* start, Player* target, int infinityInit, int infinityDist, 
 	}
 	for (int i(0); i < nbCellSquared; i++) {
     	if (obstacles->isObstacle(i%nbCell, i/nbCell)) {
-			cout << i%nbCell << i/nbCell << endl;
+			//cout << i%nbCell << i/nbCell << endl;
     	  for (int j(0); j < nbCellSquared; j++) {
-		  cout << i << j << " OBST" << endl;
+		  //cout << i << j << " OBST" << endl;
           tabCellDist[i][j] = infinityDist;
           tabCellDist[j][i] = infinityDist;
         }
     }
     tabCellDist[i][i] = 0;
   }
-	// Diag not working
-	for (int i(0); i < nbCell; i++) {
-		for (int j(0); j < nbCell - 1; j++) {	//déterminer les distances diagonales
-			diagonalDistance(i*nbCell, (i + 1)*nbCell - 2, tabCellDist, pathAngles, obstacles);
-		}
-		for (int j(1); j < nbCell; j++) {
-			diagonalDistance(i*nbCell + 1, (i + 1)*nbCell - 1, tabCellDist, pathAngles, obstacles);
-		}
-	}
-	
-	cout << "done" << endl;
+// Diag not working
+  for (int i(0); i < nbCellSquared-nbCell; i++) {
+    if (i%nbCell != nbCell - 1) {
+      diagonalDistance(i, i + nbCell + 1, tabCellDist, pathAngles, obstacles);
+    }
+    if (i%nbCell != 0) {
+      diagonalDistance(i, i + nbCell - 1, tabCellDist, pathAngles, obstacles);
+    }
+  }
+  
+  //cout << "done" << endl;
 	int g = 0;
 	while (areThereUninitialisedCases(tabCellDist, infinityInit) && g < 20 ) {
 		g++;
-		cout << "infinityInit: " << infinityInit << endl;
+		//cout << "infinityInit: " << infinityInit << endl;
 		for (int i = 0; i < tabCellDist.size(); i++){
 			for (int j = 0; j < tabCellDist[0].size(); j++){
 				cout << "(" << i << "," << j << "," << tabCellDist[i][j] << ") ";
@@ -187,8 +190,7 @@ double floyd(Player* start, Player* target, int infinityInit, int infinityDist, 
 			}
 		}
 	}
-	cout << "gggg" << endl;
-	return pathAngles[(start->getC() * nbCell) + start->getL()][(target->getC() * nbCell) + target->getL()];
+	//cout << "gggg" << endl;
 	/* !! Si start et target sont adjacents ou diagonales, on connait leur angle avant  la boucle _while_, ce qui rend
 	inutile le fait de compléter les vector<vector<double> > ! Il faudrait que, dans ces cas, floyd ne complète pas sa 
 	tâche, ce qui serait très économique.
@@ -212,63 +214,116 @@ void shortestIndirectPath(int startID, int targetID, vector<vector<double> > &ta
 }
 
 void diagonalDistance(unsigned int i, unsigned int j, vector<vector<double> > &tabCellDist, vector<vector<double> > &pathAngles, ObstacleMap* obstacles) {
-	int nbCell = obstacles->getLNb();
-	double iXPos = i/nbCell;
-	double iYPos = i%nbCell;
-	double jXPos = j/nbCell;
-	double jYPos = j%nbCell;
-	//si un Obstacle en diagonale
-  if (obstacles->isObstacle(iXPos, jYPos) xor obstacles->isObstacle(jXPos, iYPos)) {
-		tabCellDist[i][j] = 2;
-		tabCellDist[j][i] = 2;
-		if (iYPos == jYPos + 1) {
-			if (obstacles->isObstacle(jXPos, iYPos)) {
-				pathAngles[i][j] = 3*M_PI_4;
-				pathAngles[j][i] = M_PI;
-			} else {
-				pathAngles[i][j] = 0;
-				pathAngles[j][i] = M_PI_2;
-			}
-		} else {
-			if (obstacles->isObstacle(iXPos, jYPos)) {
-				pathAngles[i][j] = M_PI;
-				pathAngles[j][i] = 3*M_PI_4;
-			} else {
-				pathAngles[i][j] = M_PI_2;
-				pathAngles[j][i] = 0;
-			}
-		}
-	//si aucun Obstacle en diagonale
-	} else if (!(obstacles->isObstacle(iXPos, jYPos)) && !(obstacles->isObstacle(jXPos, iYPos))) {
-		tabCellDist[i][j] = sqrt(2);
-		tabCellDist[j][i] = sqrt(2);
-		if (iXPos == jXPos - 1) {
-			if (iYPos == jYPos + 1){
-				pathAngles[i][j] = 7*M_PI_4;
-				pathAngles[j][i] = 3*M_PI_4;
-			} else {
-				pathAngles[i][j] = M_PI_4;
-				pathAngles[j][i] = 5*M_PI_4;
-			}
-		} else {
-			if (iYPos == jYPos + 1) {
-				pathAngles[i][j] = 5*M_PI_4;
-				pathAngles[j][i] = M_PI_4;
-			} else {
-				pathAngles[i][j] = 3*M_PI_4;
-				pathAngles[j][i] = 7*M_PI_4;
-			}
-		}
-	} //si 2 Obstacles, il faut chercher mieux, donc on laisse à infinityInit
+  int nbCell = obstacles->getLNb();
+	// cout << "nbCell: " << nbCell << endl;
+  int iX = i/nbCell;
+  int iY = i%nbCell;
+  int jX = j/nbCell;
+  int jY = j%nbCell;
+
+	cout << "i: " << i << " j: " << j << " iX: " << iX << " iY: " << iY << " jX: " << jX << " jY: " << jY << endl;
+  if (iY = jY + 1) {  //j/i
+    if (iX = jX - 1) {  //i-j
+      if (obstacles->isObstacle(jY, jX-1)) {  //oj
+				cout << "a" << endl;
+        tabCellDist[i][j] = 2;
+        tabCellDist[j][i] = 2;
+        pathAngles[i][j] = 0;
+        pathAngles[j][i] = 3*M_PI_2;
+      } else if (obstacles->isObstacle(iY, iX + 1)) {  //io
+			cout << "b" << endl;
+        tabCellDist[i][j] = 2;
+        tabCellDist[j][i] = 2;
+        pathAngles[i][j] = M_PI_2;
+        pathAngles[j][i] = M_PI;
+      } else {  //õ
+			cout << "c" << endl;
+        tabCellDist[i][j] = sqrt(2);
+        tabCellDist[j][i] = sqrt(2);
+        pathAngles[i][j] = M_PI_4;
+        pathAngles[j][i] = 5*M_PI_4;
+      }
+    } else {  //j-i
+      if (obstacles->isObstacle(jY, jX + 1)) {  //jo
+			cout << "d" << endl;
+        tabCellDist[i][j] = 2;
+        tabCellDist[j][i] = 2;
+        pathAngles[i][j] = M_PI;
+        pathAngles[j][i] = 3*M_PI_2;
+      } else if (obstacles->isObstacle(iY, iX - 1)) {  //oi
+			cout << "e" << endl;
+        tabCellDist[i][j] = 2;
+        tabCellDist[j][i] = 2;
+        pathAngles[i][j] = M_PI_2;
+        pathAngles[j][i] = 0;
+      } else {  //õ
+			cout << "f" << endl;
+        tabCellDist[i][j] = sqrt(2);
+        tabCellDist[j][i] = sqrt(2);
+        pathAngles[i][j] = 3*M_PI_4;
+        pathAngles[j][i] = 7*M_PI_4;
+      }
+    }
+  } else {  //i/j
+    if (iX = jX + 1) {  //j-i
+      if (obstacles->isObstacle(jY, jX + 1)) {  //jo
+			cout << "g" << endl;
+        tabCellDist[i][j] = 2;
+        tabCellDist[j][i] = 2;
+        pathAngles[i][j] = M_PI;
+        pathAngles[j][i] = M_PI_2;
+      } else if (obstacles->isObstacle(iY, iX - 1)) {  //oi
+			cout << "h" << endl;
+        tabCellDist[i][j] = 2;
+        tabCellDist[j][i] = 2;
+        pathAngles[i][j] = 3*M_PI_2;
+        pathAngles[j][i] = 0;
+      } else {  //õ
+			cout << "i" << endl;
+        tabCellDist[i][j] = sqrt(2);
+        tabCellDist[j][i] = sqrt(2);
+        pathAngles[i][j] = 5*M_PI_4;
+        pathAngles[j][i] = M_PI_4;
+      }
+    } else {  //i-j
+		cout << "j" << endl;
+      if (obstacles->isObstacle(jY, jX - 1)) {  //oj
+        tabCellDist[i][j] = 2;
+        tabCellDist[j][i] = 2;
+        pathAngles[i][j] = 0;
+        pathAngles[j][i] = M_PI_2;
+      } else if (obstacles->isObstacle(iY, iX + 1)) {  //io
+			cout << "k" << endl;
+        tabCellDist[i][j] = 2;
+        tabCellDist[j][i] = 2;
+        pathAngles[i][j] = 3*M_PI_2;
+        pathAngles[j][i] = 0;
+      } else {  //õ
+			cout << "l" << endl;
+        tabCellDist[i][j] = sqrt(2);
+        tabCellDist[j][i] = sqrt(2);
+        pathAngles[i][j] = 7*M_PI_4;
+        pathAngles[j][i] = 3*M_PI_4;
+      }
+    }
+  }
 }
 
-double complexPath(Player* start, Player* target, int infinityInit, int infinityDist, ObstacleMap* obstacles){
+double complexPath(Player* start, Player* target, int infinityInit, int infinityDist, ObstacleMap* obstacles,bool &firstInStep){
 	/* Description of Floyd's algorithm's implementation */
-	return floyd(start, target, infinityInit, infinityDist, obstacles);
+	int nbCell = obstacles->getLNb();
+	if (firstInStep){
+		cout << "Floyd" << endl;
+		floyd(start, target, infinityInit, infinityDist, obstacles);
+		firstInStep = false;
+		return pathAngles[(start->getC() * nbCell) + start->getL()][(target->getC() * nbCell) + target->getL()];
+	}
+	return pathAngles[(start->getC() * nbCell) + start->getL()][(target->getC() * nbCell) + target->getL()];
 }
 
-double whichPath(Player* start, Player* target, int infinityInit, int infinityDist, ObstacleMap* obstacles) {
+double whichPath(Player* start, Player* target, int infinityInit, int infinityDist, ObstacleMap* obstacles, bool &firstInStep) {
 	if (isThereObstacleBetween(start->getL(), start->getC(), target->getL(), target->getC(), obstacles)) {
-		return complexPath(start, target, infinityInit, infinityDist, obstacles);
+		cout << "complex path" << endl;
+		return complexPath(start, target, infinityInit, infinityDist, obstacles, firstInStep);
 	} else {return simplePath(start, target);}
 }
--- src/pathfinding.cc
+++ src/pathfinding.cc
@@ -13,16 +13,20 @@ static int infinityInit;
 static int infinityDist;
 
 void pathfinding(PlayerMap* players, ObstacleMap* obstacles){
+    bool firstInStep = true;
     infinityDist = pow(obstacles->getCNb(), 2);
     infinityInit = infinityDist + 1;
     targets.resize(players->getNb());
     pathAngles.resize(players->getNb());
+    cout << "--------------------TARGETTING--------------------" << endl;
     targets = targetting(players, infinityInit, infinityDist);
+    cout << "--------------------WHICH-PATH--------------------" << endl;
     for (int p = 0; p < players->getNb(); p++){
         cout << "start ID: " << players->getPlayer(p)->getID() << " target ID: " << targets[p] << endl;
-        pathAngles[p] = whichPath(players->getPlayer(p), players->getPlayer(targets[p]), infinityInit, infinityDist, obstacles); 
+        pathAngles[p] = whichPath(players->getPlayer(p), players->getPlayer(targets[p]), infinityInit, infinityDist, obstacles, firstInStep); 
+        cout << "Player " << players->getPlayer(p)->getID() << " angle: " << pathAngles[p]*180/M_PI << " degrés" << endl;
+	    cout << endl;
     }
-    cout << "t" << endl;
 }
 
 double nearestPlayerAngle(int ID){
