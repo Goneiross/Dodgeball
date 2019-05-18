e8d8a6b8223b1635e2cf58e116604ba751e52fea
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-05-18 15:50:56 +0200

    Fix aStar

--- src/aStar.cc
+++ src/aStar.cc
@@ -10,18 +10,29 @@
 #include "obstacle.h"
 #include "player.h"
 #include "tools.h"
+#include "define.h"
 
 #include <iostream>
 
 using namespace std;
+
 
 typedef struct Tile {
   int l;
   int c;
 } Tile;
 
+bool operator==(Tile t1, Tile t2){
+  if (t1.c == t2.c && t1.l == t2.l){
+    return true;
+  } else {
+    return false;
+  }
+}
+
 class Node {
   public :
+    Node(Tile at);
     Node(Node* from, Tile at);
     Node(Node* const& copy);
     ~Node(){parent = nullptr;};
@@ -31,12 +42,17 @@ class Node {
     int endCost;
     int beginCost;
 };
-Node::Node(Node* from, Tile at)
-  : parent (from),
-    position (at),
+Node::Node(Tile at)
+  : position (at),
     totalCost(0),
     endCost(0),
-    beginCost(0) {}
+    beginCost(0) {parent = nullptr;}
+
+Node::Node(Node* from, Tile at)
+  : position (at),
+    totalCost(0),
+    endCost(0),
+    beginCost(0) {parent = from;}
 
 Node::Node(Node* const& copy)
   : parent (copy->parent),
@@ -45,91 +61,117 @@ Node::Node(Node* const& copy)
     endCost(copy->endCost),
     beginCost(copy->beginCost) {}
 
+/*
+Node& Node::operator=(Node const& copy) {
+    if(this != &copy) {
+        position = copy.position;
+        totalCost = copy.totalCost;
+        endCost = copy.endCost;
+        beginCost = copy.beginCost;
+        delete parent;
+        parent = new Node(copy.parent);
+    }
+    return *this;
+} */
+
+int nearestPlayer(PlayerMap *players, int ID);
+
 double whichPath(PlayerMap *players, int startID, ObstacleMap* obstacles) {
+  std::cout << "-----aStar-----" << endl;
   double pathAngle = 0;
   int targetID = nearestPlayer(players, startID);
   int LNb = players->getLNb(), CNb = players->getCNb();
+  Node* start = new Node ({players->getPlayer(startID)->getL(),players->getPlayer(startID)->getC()});
+  Node* target = new Node({players->getPlayer(targetID)->getL(),players->getPlayer(targetID)->getC()});
 
-  double **scoreGrid = new double *[LNb]; // call LNb and CNb outside !
-  for (int i = 0; i < LNb; i++) {
-    scoreGrid[i] = new double[CNb];
-    for (int j = 0; j < CNb; j++) {
-      scoreGrid[i][j] = LNb * sqrt(2); // maybe squared ? Do outisde
-    }
-  }
-  bool **openGrid = new bool *[LNb];
-  for (int i = 0; i < LNb; i++) {
-    openGrid[i] = new bool[CNb];
-    for (int j = 0; j < CNb; j++) {
-      openGrid[i][j] = true;
-    }
-  }
-
-  Node* start = new Node(nullptr,{players->getPlayer(startID)->getL(),players->getPlayer(startID)->getC()});
-  Node* target = new Node(nullptr,{players->getPlayer(targetID)->getL(),players->getPlayer(targetID)->getC()});
-  Node* current;
+  Node* current(start);
 
   vector<Node*> openList;
   vector<Node*> closedList;
   vector<Tile> path;
   openList.push_back(start);
 
-  while (openList.size() > 0) {
-    current = new Node(openList[0]);
+  std::cout << "Start " << startID << "(" << players->getPlayer(startID)->getL() << "," << players->getPlayer(startID)->getC() << ")" << endl;
+
+  do {
+    /*
+    for (int i = 0; i < openList.size(); i++){
+      Node back = openList[i];
+      while(not (back.parent->position == start.position) && i < 10){
+        i++;
+        cout << back.parent->position.l << " " << back.parent->position.c << endl;
+        path.push_back(back.position);
+        back = back.parent;
+      }
+    }
+    */
+    current = openList[0];
     int currentIndex = 0;
     for (int i = 0; i < openList.size(); i++){
       if(openList[i]->totalCost < current->totalCost){
-        current = new Node(openList[i]);
+        current = openList[i];
         currentIndex = i;
       }
     }
+    cout << "Current: " << current->position.l << " " << current->position.c << endl;
     openList.erase(openList.begin() + currentIndex);
     closedList.push_back(current);
-
-    if (current == target){
-      Node* back = new Node(current);
-      while(back->parent != nullptr){
-        path.push_back(back->position);
-        back = new Node(current->parent);
+    if (current->position == target->position){
+      cout << "Current = target" << endl;
+      Node* back = current;
+      while(not (back->parent->position == start->position)){
+        cout << "Back: " << back->position.l << " " << back->position.c << " ";
+        cout << "Parent: " << back->parent->position.l << " " << back->parent->position.c << endl;
+        path.push_back(back->parent->position);
+        back = back->parent;
       }
-      // END
-      delete back;
+      cout << "END" << endl;
+      break;
     }
     vector<Node*> children;
     for (int i = -1; i <= 1; i++){
       for (int j = -1; j <= 1; j++){
-        if (not (i == 0 && j == 0)){
+        if (not(abs(i) == abs(j))){
           Tile newPosition = {current->position.l + i, current->position.c + j};
-          if (newPosition.l < 0 || newPosition.l > obstacles->getLNb()){continue;} // > or => ???
-          if (newPosition.c < 0 || newPosition.c > obstacles->getCNb()){continue;} // > or => ???
+          if (newPosition.l < 0 || newPosition.l >= obstacles->getLNb()){continue;} // > or => ???
+          if (newPosition.c < 0 || newPosition.c >= obstacles->getCNb()){continue;} // > or => ???
           if(obstacles->isObstacle(newPosition.l,newPosition.c)){continue;}
           children.push_back(new Node(current, newPosition));
+          cout << "PUSHED: " << children[children.size()-1]->position.l << " "  << children[children.size()-1]->position.c;
+          cout << " FROM: " << children[children.size()-1]->parent->position.l << " " << children[children.size()-1]->parent->position.c << endl;
         }
       }
     }
     for (int childID = 0; childID < children.size(); childID++){
       for (int closedChildID = 0; closedChildID < closedList.size(); closedChildID++){
-        if (children[childID] == closedList[closedChildID]){continue;}
+        if (children[childID]->position == closedList[closedChildID]->position){continue;}
       }
       double lDistance = children[childID]->position.l - target->position.l;
       double cDistance = children[childID]->position.c - target->position.c;
       children[childID]->beginCost = current->beginCost + 1;
       children[childID]->endCost = pow(lDistance, 2) + pow(cDistance, 2);
       children[childID]->totalCost = children[childID]->beginCost + children[childID]->endCost;
+
+      for (int openChildID = 0; openChildID < openList.size(); openChildID++){
+        if (children[childID]->position == openList[openChildID]->position && children[childID]->beginCost > openList[openChildID]->beginCost){continue;}
+      }
+
+      openList.push_back(children[childID]);
     }
-  }
+  } while (openList.size() > 0);
+
   int nextStepID= path.size() - 1;
-  pathAngle = angle(path[nextStepID].c, path[nextStepID].l, path[nextStepID].c -1, path[nextStepID].l -1);
-  cout << "PATH ANGLE" << pathAngle << endl;
-  delete start;
-  delete target;
-  delete current;
+  cout << nextStepID << endl;
+  cout << path[nextStepID].c << endl;
+  pathAngle = angle(players->getPlayer(startID)->getC(), players->getPlayer(startID)->getL(), path[nextStepID].c -1, path[nextStepID].l -1);
+  std::cout << "PATH ANGLE" << pathAngle << endl;
+  cout << "RETURN" << endl;
   return pathAngle;
 }
 
 int nearestPlayer(PlayerMap *players, int ID) {
   int nearestID = -1;
-  double nearestDistance = players->getLNb() * 2; // better than sqrt(2)
+  double nearestDistance = SIDE * 2; // better than sqrt(2)
   for (int p = 0; p < players->getNb(); p++) {
     if (ID != p) {
       double dist = distance(players->getPlayer(ID)->getHitbox(),
