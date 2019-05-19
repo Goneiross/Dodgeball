ccee130544756dbf1c2a9aaa90eeac06e78b36e8
Goneiross <guillaume.julien.pellerin@gmail.com>
2019-05-19 15:56:43 +0200

    Rework A* structure

--- src/aStar.cc
+++ src/aStar.cc
@@ -30,121 +30,61 @@ bool operator==(Tile t1, Tile t2) {
 
 class Node {
   public:
-    Node(Tile at, Node *from = nullptr);
-    Node(Node *const &copy);
+    Node(Tile at, Node* from = nullptr);
+    Node(Node* const& copy);
     ~Node() { delete parent; };
     Tile position;
-    Node *parent;
+    Node* parent;
     int totalCost;
     int endCost;
     int beginCost;
 };
 
-Node::Node(Tile at, Node *from)
+void getBestOpenedNode(vector<Node*>& openedList, vector<Node*>& closedList,
+                       Node*& current);
+bool won(vector<Tile>& path, Node* current, Node* start, Node* target,
+         bool& foundSolution);
+void getNewChildren(ObstacleMap* obstacles, vector<Node*>& children, Node* current);
+void addChildrenToOpened(vector<Node*>& openedList, vector<Node*> closedList,
+                         vector<Node*>& children, Node* current, Node* target);
+
+Node::Node(Tile at, Node* from)
     : position(at), totalCost(0), endCost(0), beginCost(0), parent(from) {}
 
-Node::Node(Node *const &copy)
+Node::Node(Node* const& copy)
     : parent(copy->parent), position(copy->position), totalCost(copy->totalCost),
       endCost(copy->endCost), beginCost(copy->beginCost) {}
 
-double whichPath(PlayerMap *players, int startID, int targetID,
-                 ObstacleMap *obstacles) {
+double whichPath(PlayerMap* players, int startID, int targetID,
+                 ObstacleMap* obstacles) {
     double pathAngle = 0;
-    int LNb = players->getLNb(), CNb = players->getCNb();
-    Node *start = new Node(
+    Node* start = new Node(
         {players->getPlayer(startID)->getL(), players->getPlayer(startID)->getC()});
-    Node *target = new Node(
+    Node* target = new Node(
         {players->getPlayer(targetID)->getL(), players->getPlayer(targetID)->getC()});
-
-    Node *current(start);
-
-    vector<Node *> openList;
-    vector<Node *> closedList;
+    Node* current(start);
+    vector<Node*> openedList;
+    vector<Node*> closedList;
     vector<Tile> path;
-    openList.push_back(start);
-
+    openedList.push_back(start);
     bool foundSolution = false;
 
     do {
-        current = openList[0];
-        int currentIndex = 0;
-        for (int i = 0; i < openList.size(); i++) {
-            if (openList[i]->totalCost < current->totalCost) {
-                current = openList[i];
-                currentIndex = i;
-            }
-        }
-        openList.erase(openList.begin() + currentIndex);
-        closedList.push_back(current);
-        if (current->position == target->position) {
-            Node *back = current;
-            while (not(back->parent->position == start->position)) {
-                path.push_back(back->parent->position);
-                back = back->parent;
-            }
-            foundSolution = true;
-            break;
-        }
-        vector<Node *> children;
-        for (int i = -1; i <= 1; i++) {
-            for (int j = -1; j <= 1; j++) {
-                if (not(abs(i) == abs(j))) {
-                    Tile newPosition = {current->position.l + i,
-                                        current->position.c + j};
-                    if (newPosition.l < 0 || newPosition.l >= obstacles->getLNb()) {
-                        continue;
-                    }
-                    if (newPosition.c < 0 || newPosition.c >= obstacles->getCNb()) {
-                        continue;
-                    }
-                    if (obstacles->isObstacle(newPosition.l, newPosition.c)) {
-                        continue;
-                    }
-                    children.push_back(new Node(newPosition, current));
-                }
-            }
-        }
-        for (int childID = 0; childID < children.size(); childID++) {
-            bool already = false;
-            for (int closedChildID = 0; closedChildID < closedList.size();
-                 closedChildID++) {
-                if (children[childID]->position ==
-                    closedList[closedChildID]->position) {
-                    already = true;
-                }
-            }
-            if (not already) {
-                double lDistance = children[childID]->position.l - target->position.l;
-                double cDistance = children[childID]->position.c - target->position.c;
-                children[childID]->beginCost = current->beginCost + 1;
-                children[childID]->endCost = pow(lDistance, 2) + pow(cDistance, 2);
-                children[childID]->totalCost =
-                    children[childID]->beginCost + children[childID]->endCost;
+        getBestOpenedNode(openedList, closedList, current);
+        if (won(path, current, start, target, foundSolution)) { break; }
+        vector<Node*> children;
+        getNewChildren(obstacles, children, current);
+        addChildrenToOpened(openedList, closedList, children, current, target);
+    } while (openedList.size() > 0);
 
-                for (int openChildID = 0; openChildID < openList.size();
-                     openChildID++) {
-                    if (children[childID]->position ==
-                            openList[openChildID]->position &&
-                        children[childID]->beginCost >
-                            openList[openChildID]->beginCost) {
-                        continue;
-                    }
-                }
-
-                openList.push_back(children[childID]);
-            }
-        }
-    } while (openList.size() > 0);
-    int nextStepID = path.size() - 1;
+    long int nextStepID = path.size() - 1;
     if (nextStepID < 0) {
         if (foundSolution) {
             return angle(players->getPlayer(startID)->getX(),
                          players->getPlayer(startID)->getY(),
                          players->getPlayer(targetID)->getX(),
                          players->getPlayer(targetID)->getY());
-        } else {
-            return NO_SOLUTION;
-        }
+        } else { return NO_SOLUTION; }
     }
     double S = SIDE / obstacles->getLNb();
     double X = (0.5 * S + path[nextStepID].c * S) - (DIM_MAX);
@@ -152,4 +92,79 @@ double whichPath(PlayerMap *players, int startID, int targetID,
     pathAngle = angle(players->getPlayer(startID)->getX(),
                       players->getPlayer(startID)->getY(), X, Y);
     return pathAngle;
+}
+
+void getBestOpenedNode(vector<Node*>& openedList, vector<Node*>& closedList,
+                       Node*& current) {
+    current = openedList[0];
+    long unsigned int currentIndex = 0;
+    for (long unsigned int o = 0; o < openedList.size(); o++) {
+        if (openedList[o]->totalCost < current->totalCost) {
+            current = openedList[o];
+            currentIndex = o;
+        }
+    }
+    openedList.erase(openedList.begin() + currentIndex);
+    closedList.push_back(current);
+}
+
+bool won(vector<Tile>& path, Node* current, Node* start, Node* target,
+         bool& foundSolution) {
+    if (current->position == target->position) {
+        Node* back = current;
+        while (not(back->parent->position == start->position)) {
+            path.push_back(back->parent->position);
+            back = back->parent;
+        }
+        foundSolution = true;
+        return true;
+    }
+    return false;
+}
+
+void getNewChildren(ObstacleMap* obstacles, vector<Node*>& children, Node* current) {
+    for (short i = -1; i <= 1; i++) {
+        for (short j = -1; j <= 1; j++) {
+            if (not(abs(i) == abs(j))) {
+                Tile newPosition = {current->position.l + i, current->position.c + j};
+                if (newPosition.l < 0 || newPosition.l >= obstacles->getLNb()) {
+                    continue;
+                }
+                if (newPosition.c < 0 || newPosition.c >= obstacles->getCNb()) {
+                    continue;
+                }
+                if (obstacles->isObstacle(newPosition.l, newPosition.c)) { continue; }
+                children.push_back(new Node(newPosition, current));
+            }
+        }
+    }
+}
+
+void addChildrenToOpened(vector<Node*>& openedList, vector<Node*> closedList,
+                         vector<Node*>& children, Node* current, Node* target) {
+    for (long unsigned int childID = 0; childID < children.size(); childID++) {
+        bool already = false;
+        for (long unsigned int c = 0; c < closedList.size(); c++) {
+            if (children[childID]->position == closedList[c]->position) {
+                already = true;
+            }
+        }
+        if (not already) {
+            double lDistance = children[childID]->position.l - target->position.l;
+            double cDistance = children[childID]->position.c - target->position.c;
+            children[childID]->beginCost = current->beginCost + 1;
+            children[childID]->endCost = pow(lDistance, 2) + pow(cDistance, 2);
+            children[childID]->totalCost =
+                children[childID]->beginCost + children[childID]->endCost;
+
+            for (long unsigned int o = 0; o < openedList.size(); o++) {
+                if (children[childID]->position == openedList[o]->position &&
+                    children[childID]->beginCost > openedList[o]->beginCost) {
+                    continue;
+                }
+            }
+
+            openedList.push_back(children[childID]);
+        }
+    }
 }
